6682619bda58b66b65e71422952c234d32c96669
Lewis Robbins <lewis.robbins2@gmail.com>
2018-08-26 20:46:34 +0100

    add glass

--- hittable.h
+++ hittable.h
@@ -3,10 +3,13 @@
 
 #include "ray.h"
 
+class material;
+
 struct hit_records {
     float t;
     vec3 p;
     vec3 normal;
+    material *mat_ptr;
 };
 
 class hittable {
--- main.cpp
+++ main.cpp
@@ -6,13 +6,20 @@
 #include <float.h>
 #include "stb_image_write.h"
 #include "main.h"
+#include "material.h"
+#include "lambertian.h"
 
-vec3 color(const ray &r, hittable *world) {
+vec3 color(const ray &r, hittable *world, int depth) {
     hit_records rec;
-    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
-        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5*color(ray(rec.p, target - rec.p), world);
-    }
+    if (world->hit(r, 0.001, MAXFLOAT, rec)) {
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation*color(scattered, world, depth+1);
+        } else {
+            return vec3(0,0,0);
+        }
+    } 
     vec3 unit_direction = unit_vector(r.direction());
     float t = 0.5*(unit_direction.y() + 1.0);
     return (1.0-t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0);
@@ -28,15 +35,17 @@ vec3 random_in_unit_sphere() {
 
 int main(void)
 {
-    int nx = 1280;
-    int ny = 720;
+    int nx = 800;
+    int ny = 600;
     int ns = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    hittable *list[2];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hittable *list[4];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1,0,-1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+    list[3] = new sphere(vec3(-1,0,-1),0.5, new metal(vec3(0.8, 0.8,0.8)));
     camera cam;
-    hittable *world = new hittable_list(list, 2);
+    hittable *world = new hittable_list(list, 4);
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
             vec3 col(0,0,0);
@@ -44,9 +53,10 @@ int main(void)
                 float u = float(i+drand48()) / float(nx);
                 float v = float(j+drand48()) / float(ny);
                 ray r = cam.get_ray(u, v);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
             col /= float(ns);
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
             int ir = int(255.99*col[0]);
             int ig = int(255.99*col[1]);
             int ib = int(255.99*col[2]);
