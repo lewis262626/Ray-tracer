f8336a8a3471d9cd6cdb4c4467608e5cb03e844a
Lewis Robbins <lewis.robbins2@gmail.com>
2018-08-26 12:03:25 +0100

    done antialiasing

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "sphere.h"
 #include "hittable_list.h"
 #include "vec3.h"
+#include "camera.h"
 #include <float.h>
 
 vec3 color(const ray &r, hittable *world) {
@@ -18,29 +19,28 @@ int main(void)
 {
     int nx = 800;
     int ny = 600;
+    int ns = 100;
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
     hittable *list[2];
     list[0] = new sphere(vec3(0, 0, -1), 0.5);
     list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    camera cam;
     hittable *world = new hittable_list(list, 2);
     for (int j = ny-1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner +u*horizontal + v*vertical);
-            
-            vec3 p = r.point_at_parameter(2.0);
-            vec3 col = color(r, world);
+            vec3 col(0,0,0);
+            for (int s = 0; s < ns; s++) {
+                float u = float(i+drand48()) / float(nx);
+                float v = float(j+drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                col += color(r, world);
+            }
+            col /= float(ns);
             int ir = int(255.99*col[0]);
-            int ig = int(255.9*col[1]);
-            int ib = int(255.9*col[2]);
+            int ig = int(255.99*col[1]);
+            int ib = int(255.99*col[2]);
             std::cout << ir << " " << ig << " " << ib << "\n";
         }
     }
-
     return 0;
 }
